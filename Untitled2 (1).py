944de23d689a6046e4f8a20c166cafb6aca63784
bencloutierr <104613988+bencloutierr@users.noreply.github.com>
2022-09-03 19:27:58 -0400

    Add files via upload

---
+++ Untitled2 (1).py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+print("Welcome to Romeo and Juliet! This is the prologue, enter the words as described and realize your Shakespearean passions! \n")
+
+
+one = input("Noun(plural): ")
+two = input("Place: ")
+three = input("Noun: ")
+four = input("Noun(plural): ")
+five = input("Noun: ")
+six = input("Adjective: ")
+seven = input("Verb: ")
+eight = input("Number: ")
+nine = input("Adjective: ")
+ten = input("Body Part: ")
+eleven = input("Verb: ")
+
+print(f"Two {one}, both alike in dignity, \n In fair {two}, where we lay our scene, \n From ancient {three} break to new mutiny, \n Where civil blood makes civil hands unclean. \n From forth the fatal loins of these two foes \n A pair of star-cross`d {four} take their life; \n Whole misadventured piteous overthrows \n Do with their {five} bury their parents` strife. \n The fearful passage of their {six} love, \n And the continuance of their parents` rage, \n Which, but their children`s end, nought could {seven}, \n Is now the {eight} hours` traffic of our stage; \n The which if you with {nine} {ten} attend, \n What here shall {eleven}, our toil shall strive to mend.")
+
+
+# In[ ]:
+
+
+
+
